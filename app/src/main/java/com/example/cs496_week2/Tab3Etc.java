4fa3e188e284b482642b50f119fa13a3fec68b80
kimjungwow <jw950310@kaist.ac.kr>
2019-01-03 11:18:23 +0900

    Make three tabs

---
+++ app/src/main/java/com/example/cs496_week2/Tab3Etc.java
@@ -0,0 +1,21 @@
+package com.example.cs496_week2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab3Etc extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.tab3etc, container, false);
+
+
+        return rootView;
+    }
+
+
+}
