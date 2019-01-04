b77dd04d13f2e9e117ee15cae2b0a3a0dec5b104
kimjungwow <jw950310@kaist.ac.kr>
2019-01-05 01:18:23 +0900

    Tab 1 : HTTP PUT done

--- app/src/main/java/com/example/cs496_week2/Tab1Phonebook.java
+++ app/src/main/java/com/example/cs496_week2/Tab1Phonebook.java
@@ -2,6 +2,7 @@ package com.example.cs496_week2;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.app.DownloadManager;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -39,6 +40,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Bitmap;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Downloader;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -70,9 +81,14 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     String[] REQUIRED_PERMISSIONS = {Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS};
     public int sel_pos = -1;
+    private JSONObject big;
 
+    private String urlstring = "http://socrip3.kaist.ac.kr:9080/api/contacts";
     private FloatingActionButton msgButton;
     private FloatingActionButton addButton;
+    private RequestQueue mQueue;
+    private URL url;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -80,6 +96,13 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
         Log.i("전화번호부 fragment", "onCreateView()");
         View rootView = inflater.inflate(R.layout.tab1phonebook, container, false);
         tvData = (TextView) getActivity().findViewById(R.id.tvData);
+
+        mQueue = Volley.newRequestQueue(getContext());
+
+
+
+
+
 
 
         contactsListView = rootView.findViewById(R.id.contactLV);
@@ -89,7 +112,6 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
 
         msgButton = (FloatingActionButton) rootView.findViewById(R.id.messageButton);
         addButton = rootView.findViewById(R.id.addContactButton);
-
 
 
         return rootView;
@@ -110,6 +132,7 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
         contactsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
                 if (sel_pos == position) {
                     contactsListView.setAdapter(adapter);
                     sel_pos = -1;
@@ -119,14 +142,67 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
                     sel_pos = position;
                 }
 
+//                jsonParse();
+
+
+
+
+//                HttpURLConnection httpURLConnection = null;
+//                try{
+
+//                    httpURLConnection = (HttpURLConnection) url.openConnection();
+//
+//                    httpURLConnection.setRequestMethod("GET"); // URL 요청에 대한 메소드 설정 : POST.
+////                    httpURLConnection.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset 설정.
+////                    httpURLConnection.setRequestProperty("Context_Type", "application/x-www-form-urlencoded;cahrset=UTF-8");
+//                    httpURLConnection.setReadTimeout(10000);
+//                    httpURLConnection.setConnectTimeout(10000);
+//                    httpURLConnection.setDoOutput(true);
+////                    httpURLConnection.setDoInput(true);
+//
+//
+//                    httpURLConnection.connect();
+//
+//
+////                    InputStream is = httpURLConnection.getInputStream(); //input스트림 개방
+//
+//
+//
+//                    BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+//                    StringBuilder builder = new StringBuilder(); //문자열을 담기 위한 객체
+////                    BufferedReader reader = new BufferedReader(new InputStreamReader(is)); //문자열 셋 세팅
+////                    BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8")); //문자열 셋 세팅
+//
+//                    String line;
+//
+//                    while ((line = reader.readLine()) != null) {
+//                        builder.append(line+ "\n");
+//                    }
+//                    reader.close();
+//
+//                    String result = builder.toString();
+////                    Toast.makeText(getContext(), result, Toast.LENGTH_LONG).show();
+//
+//
+//
+//
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } finally {
+//                    if (httpURLConnection != null) {
+//                        httpURLConnection.disconnect();
+//                    }
+//                }
+
             }
 
         });
         msgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (contactsListView.getCheckedItemCount() != 0 && sel_pos!=-1) {
-
+                if (contactsListView.getCheckedItemCount() != 0 && sel_pos != -1) {
 
 
                     Uri smsUri;
@@ -155,8 +231,6 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
                     } else {
                         intent = new Intent(Intent.ACTION_VIEW, smsUri);
                     }
-
-
                     contactsListView.clearChoices();
                     startActivity(intent);
 
@@ -174,158 +248,19 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
-                if (WritePermissioncheck()) {
-                    Intent intent = new Intent(getActivity().getApplicationContext(), com.example.cs496_week2.AddContactActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(getContext(), "Cannot add contact.", Toast.LENGTH_SHORT).show();
-                }
-
-
+//                if (WritePermissioncheck()) {
+//                    Intent intent = new Intent(getActivity().getApplicationContext(), com.example.cs496_week2.AddContactActivity.class);
+//                    startActivity(intent);
+//                } else {
+//                    Toast.makeText(getContext(), "Cannot add contact.", Toast.LENGTH_SHORT).show();
+//                }
+                jsonParse();
             }
         });
-
-
-
-    }
-    public class JSONTask extends AsyncTask<String, String, String> {
-
-        @Override
-
-        protected String doInBackground(String[] urls) {
-
-            try {
-
-                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
-
-                JSONObject jsonObject = new JSONObject();
-
-                jsonObject.accumulate("user_id", "androidTest");
-
-                jsonObject.accumulate("name", "yun");
-
-                HttpURLConnection con = null;
-
-                BufferedReader reader = null;
-
-                try{
-
-                    //URL url = new URL(“http://192.168.25.16:3000/users“);
-
-                    URL url = new URL(urls[0]);
-
-                    //연결을 함
-
-                    con = (HttpURLConnection) url.openConnection();
-
-                    con.setRequestMethod("POST");//POST방식으로 보냄
-
-                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
-
-                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
-
-                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
-
-                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
-
-                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
-
-                    con.connect();
-
-                    //서버로 보내기위해서 스트림 만듬
-
-                    OutputStream outStream = con.getOutputStream();
-
-                    //버퍼를 생성하고 넣음
-
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
-
-                    writer.write(jsonObject.toString());
-
-                    writer.flush();
-
-                    writer.close();//버퍼를 받아줌
-
-                    //서버로 부터 데이터를 받음
-
-                    InputStream stream = con.getInputStream();
-
-                    reader = new BufferedReader(new InputStreamReader(stream));
-
-                    StringBuffer buffer = new StringBuffer();
-
-                    String line = "";
-
-                    while((line = reader.readLine()) != null){
-
-                        buffer.append(line);
-
-                    }
-
-                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
-
-                } catch (MalformedURLException e){
-
-                    e.printStackTrace();
-
-                } catch (IOException e) {
-
-                    e.printStackTrace();
-
-                } finally {
-
-                    if(con != null){
-
-                        con.disconnect();
-
-                    }
-
-                    try {
-
-                        if(reader != null){
-
-                            reader.close();//버퍼를 닫아줌
-
-                        }
-
-                    } catch (IOException e) {
-
-                        e.printStackTrace();
-
-                    }
-
-                }
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
-            }
-
-            return null;
-
-        }
-
-        @Override
-
-        protected void onPostExecute(String result) {
-
-            super.onPostExecute(result);
-
-            tvData.setText(result);//서버로 부터 받은 값을 출력해주는 부
-
-        }
-
-    }
-
+}
 
     public int checkselfpermission(String permission) {
         return PermissionChecker.checkSelfPermission(getContext(), permission);
-
-
     }
 
     public boolean Permissioncheck() {
@@ -341,6 +276,45 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
                 return false;
             }
         }
+    }
+    public void jsonParse() {
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, urlstring, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("TAG", response.toString());
+
+
+                try {
+                    JSONArray contacts = response;
+
+
+                    Log.d("JsonArray",contacts.toString());
+                    for(int i=0;i<contacts.length();i++){
+                        JSONObject jresponse = contacts.getJSONObject(i);
+
+                        String name = jresponse.getString("name");
+                        Log.d("nickname",name);
+                        String number = jresponse.getString("number");
+                        if(i==0)
+                        Toast.makeText(getContext(),name+number,Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        });
+        mQueue.add(request);
+
     }
 
     public boolean WritePermissioncheck() {
@@ -400,13 +374,15 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
 
                 //add contact information in form of JSONObject to jsonArr
 
+
                 JSONObject obj = new JSONObject();
                 try {
-
                     obj.put("name", name);
                     obj.put("number", phoneNumber);
-//                    obj.put("img", bp);
+//                    obj.put("img", getStringFromBitmap(bp));
                     jsonArr.add(obj);
+
+
                     new SendDeviceDetails().execute("http://socrip3.kaist.ac.kr:9080/api/contacts", obj.toString());
 
                 } catch (JSONException e) {
@@ -414,15 +390,21 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
                 }
 
 
-
             }
             phones.close();
         }
 
+//        big.put("contacts", jsonArr);
+
+
+
+
+
 
         File firstmyfile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 
-        File myfile = new File(firstmyfile, "json_phonebook.txt");
+        File myfile = new File(firstmyfile, "json_phonebook2.txt");
+
         try (FileWriter fileWriter = new FileWriter(myfile)) {
             String jsonstring;
             for (JSONObject s : jsonArr) {
@@ -432,6 +414,12 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
                 fileWriter.append(jsonstring);
 
             }
+//            jsonstring=big.toString();
+//            fileWriter.append(jsonstring);
+
+//            new SendDeviceDetails().execute("http://socrip3.kaist.ac.kr:9080/api/contacts", obj.toString());
+
+
 
         } catch (IOException e) {
             //Handle exception
@@ -442,6 +430,30 @@ public class Tab1Phonebook extends Fragment implements ActivityCompat.OnRequestP
             LV.setAdapter(adapter);
         }
         return;
+    }
+
+    public String getStringFromBitmap(Bitmap bitmapPicture) {
+        /*
+         * This functions converts Bitmap picture to a string which can be
+         * JSONified.
+         * */
+        final int COMPRESSION_QUALITY = 100;
+        String encodedImage;
+        ByteArrayOutputStream byteArrayBitmapStream = new ByteArrayOutputStream();
+        bitmapPicture.compress(Bitmap.CompressFormat.PNG, COMPRESSION_QUALITY,
+                byteArrayBitmapStream);
+        byte[] b = byteArrayBitmapStream.toByteArray();
+        encodedImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    public Bitmap getBitmapFromString(String jsonString) {
+        /*
+         * This Function converts the String back to Bitmap
+         * */
+        byte[] decodedString = Base64.decode(jsonString, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        return decodedByte;
     }
 
     @Override
