3611a93a2a4a6504f9f4456d6022ac2677098dc2
kimjungwow <jw950310@kaist.ac.kr>
2019-01-06 20:53:01 +0900

    Third tab - sign in || sign up

---
+++ app/src/main/java/com/cs496/cs496_week2/Tab3Game.java
@@ -0,0 +1,14 @@
+package com.cs496.cs496_week2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class Tab3Game extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab3game);
+    }
+
+
+}
